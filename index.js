04fe453d359d31cb75382ae55f13f5da461b9e5c
Asjad Nawaz Khan <asjadnawaz2002@gmail.com>
2024-03-25 01:14:10 +0500

    Update

---
+++ index.js
@@ -0,0 +1,6 @@
+var number1 = 10;
+var number2 = 78;
+var number3 = 45;
+var total = ++number1 + ++number1 + number2-- + ++number3 + number1 + number1-- + number1 + --number3;
+console.log(total);
+export {};
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
 import inquirer from "inquirer";
+
+var answer = await inquirer.prompt([{
+    message: "Please guess a number",
+    name: "Number",
+    type: "number"
+}])
+
+var randomNumber: number = Math.floor(Math.random() * 10 + 1);
+
+if (answer.Number === randomNumber) {
+    console.log("Congratulations! You got the right Number");
+} else { console.log("You guess wrong number")}
