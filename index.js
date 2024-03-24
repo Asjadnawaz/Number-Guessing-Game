6226931f403d90f2158b096b210d7a4e620ac893
Asjad Nawaz Khan <asjadnawaz2002@gmail.com>
2024-03-25 01:28:51 +0500

    Updated

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
 var answer = await inquirer.prompt([{
-        message: "Please guess a number between 1 to 10",
+        message: "Please guess a number between 1 to 10:",
         name: "Number",
         type: "number"
     }]);
 var randomNumber = Math.floor(Math.random() * 10 + 1);
 if (answer.Number === randomNumber) {
-    console.log(chalk.greenBright.bold("Congratulations") + "!" + " " + "You got the right Number");
+    console.log(chalk.greenBright.bold("Congratulations") + "!" + " " + "You guess a right Number");
 }
 else {
     console.log(chalk.redBright.bold("You guess a wrong number"));
